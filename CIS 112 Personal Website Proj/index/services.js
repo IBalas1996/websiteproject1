f5c9b6380c586052ef3b6c3567b75ff4d4a22cb1
IBalas1996 <31708508+IBalas1996@users.noreply.github.com>
2017-12-18 15:19:34 -0500

    Add files via upload
    Project 1 Complete! Definitely a lot to improve on but it's a start!

---
+++ CIS 112 Personal Website Proj/index/services.js
@@ -0,0 +1,15 @@
+
+ function startLightBox(){
+			var lbBg = document.getElementById("lightBoxBg");
+			var lb = document.getElementById("lightBox");
+
+			lbBg.style.display = "block";
+			lb.style.display = "block";
+	}
+
+	function dismiss(){
+		var lbBg = document.getElementById("lightBoxBg");
+			var lb = document.getElementById("lightBox");
+			lbBg.style.display = "none";
+			lb.style.display = "none";
+	}
